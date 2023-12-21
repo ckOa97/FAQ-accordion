851d9b2e592dba41d816662a3e16c959164ae666
Aleksandar Stankovic <aca97.vranje@gmail.com>
2023-12-21 20:37:18 +0100

    FAQ component v1.5

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ const faqElements = document.querySelectorAll('#faq-content-section > div');
 faqElements.forEach(el => {
     el.addEventListener('click', function () {
 
-        if (this.querySelector('p').classList.contains('display-none')) {
+        if(this.querySelector('p').classList.contains('display-none')) {
             this.querySelector('p').classList.remove('display-none');
+            this.querySelector('p').classList.add('display-yes');
             this.querySelector('span').classList.remove('icon-plus');
             this.querySelector('span').classList.add('icon-minus');
         } else {
+            this.querySelector('p').classList.remove('display-yes');
             this.querySelector('p').classList.add('display-none');
             this.querySelector('span').classList.remove('icon-minus');
             this.querySelector('span').classList.add('icon-plus');
